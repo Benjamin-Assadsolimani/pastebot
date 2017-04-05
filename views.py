9f40210781181ad291e515e1a170fb0757d7d336
Benjamin Assadsolimani <benjamin.assadsolimani@rwth-aachen.de>
2017-04-06 01:26:49 +0200

    added username control

--- views.py
+++ views.py
@@ -1,18 +1,18 @@
-from flask import render_template
+from flask import render_template, request
 from __init__ import app, db
 from models import Paste
 
 @app.route("/")
 def index():
-    return render_template('index.html', nav= get_navbar())
+    return render_template('index.html', glob_vars= get_glob_vars())
 
 @app.route("/paste/<int:paste_id>")
 def show_paste(paste_id):
     paste= Paste.query.get(paste_id)
     if paste == None:
-        return render_template('error.html', reason="Paste doesn't exist.", nav= get_navbar())
+        return index()
     else:
-        return render_template('paste.html', paste= paste, nav= get_navbar())
+        return render_template('paste.html', paste= paste, glob_vars= get_glob_vars())
     
 @app.route('/paste/<int:paste_id>/remove/')
 def remove_paste(paste_id):
@@ -25,6 +25,8 @@ def remove_paste(paste_id):
         return "1"
     
     
-def get_navbar():
-    nav= Paste.query.order_by(Paste.category).all()
-    return nav
+def get_glob_vars():
+    glob_vars= {}
+    glob_vars["nav"]= Paste.query.order_by(Paste.category).all()
+    glob_vars["username"]= request.cookies.get('username')
+    return glob_vars
