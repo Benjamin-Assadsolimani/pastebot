9770e6796678a2c6bd8fdda9a70193a3d01f217c
Benjamin Assadsolimani <benjamin.assadsolimani@rwth-aachen.de>
2017-04-08 15:54:13 +0200

    added simple database archiving

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DATABASE_NAME= os.path.join(basedir, 'pastebot.db')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_NAME
-SQLALCHEMY_TRACK_MODIFICATIONS= False
+DATABASE_ARCHIVE                = os.path.join(basedir, 'db.old')
+DATABASE_NAME                   = 'pastebot.db'
+DATABASE_PATH                   = os.path.join(basedir, DATABASE_NAME)
+SQLALCHEMY_DATABASE_URI         = 'sqlite:///' + DATABASE_PATH
+SQLALCHEMY_TRACK_MODIFICATIONS  = False
 
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'e124310d6e706a3cb797baa66d2f1706a3cb797d8769221472'
+WTF_CSRF_ENABLED                = True
+SECRET_KEY                      = 'e124310d6e706a3cb797baa66d2f1706a3cb797d8769221472'
--- db_create.py
+++ db_create.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 import os
+import datetime
 from __init__ import db
-from config import DATABASE_NAME
+from config import DATABASE_ARCHIVE, DATABASE_PATH, DATABASE_NAME
 
-def create_db():
-    if not os.path.exists(DATABASE_NAME):
+def archive_db():
+    if os.path.exists(DATABASE_PATH):
+        if not os.path.exists(DATABASE_ARCHIVE):
+            os.makedirs(DATABASE_ARCHIVE)
+        os.rename(DATABASE_PATH, os.path.join(DATABASE_ARCHIVE, DATABASE_NAME+"-"+str(datetime.datetime.now())))
+        print("created database backup!")
+
+def create_db(force= False):
+    if (not os.path.exists(DATABASE_PATH)) or force:
+        archive_db()
         print "Creating new database!"
         db.create_all()
 
 if __name__ == "__main__":
-    create_db()
+    create_db(True)
