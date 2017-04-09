8b4b25f80a8ec8dd419b8e5e74b466d5e70f3a01
Benjamin Assadsolimani <benjamin.assadsolimani@rwth-aachen.de>
2017-04-09 13:14:46 +0200

    models and forms are now snyced s.t. we can populate a form with the respective db class

--- forms.py
+++ forms.py
@@ -1,11 +1,41 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, PasswordField, validators
+from wtforms import StringField, validators
 from wtforms.validators import DataRequired
 from wtforms.fields.simple import TextAreaField
-from wtforms.fields.core import IntegerField
+from wtforms.fields.core import IntegerField, FieldList, FloatField
+from poppler import FormField
 
+class ModuleForm(FlaskForm):
+    name        = StringField('name', [validators.Length(min=1, max=40)])
+    content     = TextAreaField('content', [validators.Length(min=1, max=5000)])
+    score       = FloatField('score')
+    module_id   = IntegerField('module_id')
+    
+    def populate(self, module):
+        self.name       = module.name
+        self.content    = module.content
+        self.score      = module.score
+        self.module_id  = module.module_id
+        
+    
 class PasteForm(FlaskForm):
     id          = IntegerField('id')
-    author      = StringField('author', [validators.Length(min=1, max=20)])
     name        = StringField('name', [validators.Length(min=1, max=20)])
     content     = TextAreaField('content', [validators.Length(min=1, max=5000)])
+    author      = StringField('author', [validators.Length(min=1, max=20)])
+    modules     = FieldList(FormField(ModuleForm))
+    
+    def populate(self, paste):
+        self.id         = paste.id
+        self.name       = paste.name
+        self.content    = paste.content
+        self.author     = paste.author
+        
+        for module in paste.modules:
+            module_form = ModuleForm()
+            module_form.populate(module)
+            self.modules.append_entry(module_form)
+    
+
+    
+    
--- models.py
+++ models.py
@@ -2,35 +2,33 @@ from __init__ import db
 
 class Paste(db.Model):
     id      = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(5000))
-    category= db.Column(db.String(40))
     name    = db.Column(db.String(20))
+    content = db.Column(db.String(5000))
     author  = db.Column(db.String(20))
     modules = db.relationship('Module', backref='paste', order_by='desc(Module.score)', lazy='dynamic')
 
-    
-
-    def __init__(self, content, category= "general", name= "", author= "anonymous"):
-        self.content = content
-        self.category= category
-        self.author  = author
+    def __init__(self, content, name= "", author= "anonymous"):
         self.name    = name
+        self.content = content
+        self.author  = author
         
     def __repr__(self):
         return '<Paste %r (%r) by %r>' % (self.name, self.id, self.author)
     
 class Module(db.Model):
-    id      = db.Column(db.Integer, primary_key=True)
-    name    = db.Column(db.String(40))
-    score   = db.Column(db.Integer)
-    content = db.Column(db.String(5000))
-    paste_id= db.Column(db.Integer, db.ForeignKey('paste.id'))
+    id          = db.Column(db.Integer, primary_key=True)
+    name        = db.Column(db.String(40))
+    content     = db.Column(db.String(5000))
+    score       = db.Column(db.Integer)
+    module_id   = db.Column(db.Integer)
+    paste_id    = db.Column(db.Integer, db.ForeignKey('paste.id'))
     
-    def __init__(self, name, score, content, paste_id):
-        self.name= name
-        self.score= score
-        self.content= content
-        self.paste_id= paste_id
+    def __init__(self, name, score, content, module_id, paste_id):
+        self.name       = name
+        self.content    = content
+        self.score      = score
+        self.module_id  = module_id
+        self.paste_id   = paste_id
     
     def __repr__(self):
         return '<Module %r (%r) with score %r, connected to paste %r>' % (self.name, self.id, self.score, self.paste_id)
