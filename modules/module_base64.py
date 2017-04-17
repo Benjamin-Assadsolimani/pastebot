96bd7ca77da6b0c1e1a17068b04ad0048dc2fab6
Benjamin Assadsolimani <benjamin.assadsolimani@rwth-aachen.de>
2017-04-17 12:06:19 +0200

    fixed logo + improved base64 decoding module (can decode multiple b64 in one paste

--- modules/module_base64.py
+++ modules/module_base64.py
@@ -4,25 +4,30 @@ import re
 
 class Module():
     def __init__(self):
-        self.regex= re.compile("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$")
-        
+        self.regex= re.compile("([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)")
         
     def name(self):
         return "Base64-Decode";
     
     def match(self, text):
-        res= self.regex.match(text)
-        if res != None:
-            return 1
-        else:
-            return 0
+        matches= self.regex.finditer(text)
+        count= 0
+        for match in matches:
+            count+= match.end()-match.start()
+        
+        return (count/len(text))
+        
     
     def process(self, text):
+        text= text.encode("utf8")
+        return self.regex.sub(self.convert, text)
+    
+    def convert(self, matchobj):
+        text= matchobj.group(0)    
         try:
-            return b64decode(text.encode("utf8"))
+            return b64decode(text)
         except:
-            return "invalid base64!"
-        
+            return text
         
     def test(self):
         pass
